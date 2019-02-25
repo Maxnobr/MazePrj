591122ab7b26abca72ceb2b3a74939e3d37323da
Maxnobr <maxnobrsasha@gmail.com>
2019-02-25 00:30:54 -0700

    init commit

--- src/GordonSC2.java
+++ src/GordonSC2.java
@@ -2,7 +2,10 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ *
+ * version = 1
  */
+
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
